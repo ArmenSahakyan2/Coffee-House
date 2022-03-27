6c61b9cefbdb94980bca04cc30919e8fc4329adf
ArmenSahakyan2 <77206529+ArmenSahakyan2@users.noreply.github.com>
2022-03-27 12:51:15 -0700

    This is our new Coffee website :)

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let LoginForm = document.querySelector('.login-form')
+
+document.querySelector('#login-btn').onclick = () =>{
+    LoginForm.classList.add('active');
+}
+document.querySelector('#close-login-form').onclick = () =>{
+    LoginForm.classList.remove('active');
+}
+
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .nav');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+ window.onscroll = () =>{
+    LoginForm.classList.remove('active')
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+ 
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+
+}
